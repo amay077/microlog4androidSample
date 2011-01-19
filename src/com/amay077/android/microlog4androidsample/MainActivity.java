f2a410b3047fadc945cfc71838eaef59a230a33a
amay077 <okuokuoku@gmail.com>
2011-01-19 23:17:16 +0900

    Pattern Formatter を使ったサンプル

--- src/com/amay077/android/microlog4androidsample/MainActivity.java
+++ src/com/amay077/android/microlog4androidsample/MainActivity.java
@@ -5,6 +5,7 @@ import com.google.code.microlog4android.Logger;
 import com.google.code.microlog4android.LoggerFactory;
 import com.google.code.microlog4android.appender.FileAppender;
 import com.google.code.microlog4android.appender.LogCatAppender;
+import com.google.code.microlog4android.format.PatternFormatter;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -20,13 +21,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
 
         // initialize logger
+        PatternFormatter formatter = new PatternFormatter();
+        formatter.setPattern("%d{ISO8601} [%P] %m %T");
         logger.setLevel(Level.INFO);
         // write to LogCat
-        logger.addAppender(new LogCatAppender());
+        LogCatAppender logCatAppender = new LogCatAppender();
+        logCatAppender.setFormatter(formatter);
+        logger.addAppender(logCatAppender);
         // write to text file of SD-card.(need WRITE_EXTERNAL_STORAGE permission)
         FileAppender fileAppender = new FileAppender();
         fileAppender.setAppend(true);
         fileAppender.setFileName("microlog4android.log");
+        fileAppender.setFormatter(formatter);
         logger.addAppender(fileAppender);
 
         // test logging
